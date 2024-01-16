46f50c53994c09f102538e4f305da58face07761
astrosuka <yegresvoselok@gmail.com>
2024-01-16 16:09:34 -0300

    Add function that renders the library content of each item on a card

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const myLibrary = [];
+const myLibrary = [{
+    "title": "Señor de los anillos",
+    "author": "J.R.R. Tolkien",
+    "pages": "1000000",
+    "read": false
+},
+{
+    "title": "Miles de ojos",
+    "author": "Maximiliano Barrientos",
+    "pages": "248",
+    "read": true
+}
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,21 +23,48 @@ function addBookToLibrary() {
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
-    let read = document.querySelector('#read').checked;
-    
+    let read = document.querySelector('#read').checked; 
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-function createCard(){
-    for (let i = 0; i < myLibrary.length; i++){
-        document.querySelector('.card-title').textContent = myLibrary[i].title;
-        document.querySelector('.card-author').textContent = myLibrary[i].author;
-        document.querySelector('.card-pages').textContent = myLibrary[i].pages;
-        document.querySelector('.card-read').textContent = myLibrary[i].read;
+const wrapper = document.querySelector('.wrapper');
+const indexRendered =[];
+function makeCard(){
+    for (const book in myLibrary) {
+        if (!indexRendered.includes(book)){
+            let newCard = document.createElement('div');
+            newCard.classList.add('card');
+            wrapper.appendChild(newCard);
+            newCard.setAttribute('id', book);
+
+            let cardTitle = document.createElement('div');
+            cardTitle.classList.add('card-title');
+            newCard.appendChild(cardTitle);
+            cardTitle.textContent = myLibrary[book].title;
+
+            let cardAuthor = document.createElement('div');
+            cardAuthor.classList.add('card-author');
+            newCard.appendChild(cardAuthor);
+            cardAuthor.textContent = myLibrary[book].author;
+
+            let cardPages = document.createElement('div');
+            cardPages.classList.add('card-pages');
+            newCard.appendChild(cardPages);
+            cardPages.textContent = `pages: ${myLibrary[book].pages}`;
+
+            let cardRead = document.createElement('div');
+            cardRead.classList.add('card-read');
+            newCard.appendChild(cardRead);
+            cardRead.textContent = `read: ${myLibrary[book].read}`;
+
+            indexRendered.push(wrapper.children[book].attributes['id'].nodeValue)
+        }
     }
 }
 
 document.querySelector('#submit').addEventListener('click', function(){
     addBookToLibrary();
-    createCard();    
+    makeCard();    
 });
+
+makeCard();    
